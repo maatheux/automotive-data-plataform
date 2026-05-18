c790437d10a57ca86c5ee0e0429c5c8934bca6aa
Matheus Lima <matheusmarinho.lima@hotmail.com>
2026-05-17 23:45:10 -0300

    feat: dag upgrade

--- airflow/dags/postgres_to_snowflake_dag.py
+++ airflow/dags/postgres_to_snowflake_dag.py
@@ -18,43 +18,72 @@ default_args = {
     default_args=default_args,
     description='Load data incrementally from Postgres to Snowflake',
     schedule=timedelta(days=1),
-    catchup=False
+    catchup=False,
+    max_active_runs=1
 )
 def postgres_to_snowflake_etl():
+
+    @task
+    def get_max_primary_key(table_name: str):
+        with SnowflakeHook(snowflake_conn_id='snowflake').get_conn() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f'SELECT MAX(ID_{table_name}) FROM {table_name}')
+                max_id = cursor.fetchone()[0]
+                return max_id if max_id is not None else 0
+    
+    @task
+    def load_incremental_data(table_name: str, max_id: int):
+        primary_key = f'ID_{table_name}'
+        BATCH_SIZE = 10000
+        
+        with PostgresHook(postgres_conn_id='postgres').get_conn() as pg_conn:
+            with pg_conn.cursor() as pg_cursor:
+
+                pg_cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'")
+                columns = [row[0] for row in pg_cursor.fetchall()]
+                columns_list_str = ', '.join(columns)
+                placeholders = ', '.join(['%s'] * len(columns))
+
+                pg_cursor.execute(f"SELECT {columns_list_str} FROM {table_name} WHERE {primary_key} > %s", (max_id,))
+                
+                sf_hook = SnowflakeHook(snowflake_conn_id='snowflake')
+                sf_conn = sf_hook.get_conn()
+
+                try:
+                    sf_conn.auto_commit = False
+                    total_inserted = 0
+
+                    with sf_conn.cursor() as sf_cursor:
+                        insert_query = f"INSERT INTO {table_name} ({columns_list_str}) VALUES ({placeholders})"
+                        
+                        while True:
+                            rows = pg_cursor.fetchmany(BATCH_SIZE)
+                            if not rows:
+                                break
+
+                            sf_cursor.executemany(insert_query, rows)
+                            total_inserted += len(rows)
+                    
+                    if total_inserted == 0:
+                        print(f"Nenhum dado novo para tabela {table_name}.")
+                        sf_conn.rollback()
+                        return
+                    
+                    sf_conn.commit()
+                    print(f"{len(rows)} linhas inseridas em {table_name}.")
+                
+                except Exception as e:
+                    sf_conn.rollback()
+                    raise RuntimeError(f"Falha ao inserir em {table_name}: {e}") from e
+                
+                finally:
+                    sf_conn.close()
+
     table_names = ['veiculos', 'estados', 'cidades', 'concessionarias', 'vendedores', 'clientes', 'vendas']
 
     for table_name in table_names:
-        @task(task_id=f'get_max_id_{table_name}')
-        def get_max_primary_key(table_name: str):
-            with SnowflakeHook(snowflake_conn_id='snowflake').get_conn() as conn:
-                with conn.cursor() as cursor:
-                    cursor.execute(f'SELECT MAX(ID_{table_name}) FROM {table_name}')
-                    max_id = cursor.fetchone()[0]
-                    return max_id if max_id is not None else 0
-        
-        @task(task_id=f'load_data_{table_name}')
-        def load_incremental_data(table_name: str, max_id: int):
-            with PostgresHook(postgres_conn_id='postgres').get_conn() as pg_conn:
-                with pg_conn.cursor() as pg_cursor:
-                    primary_key = f'ID_{table_name}'
-
-                    pg_cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'")
-                    columns = [row[0] for row in pg_cursor.fetchall()]
-                    columns_list_str = ', '.join(columns)
-                    placeholders = ', '.join(['%s'] * len(columns))
-
-                    pg_cursor.execute(f"SELECT {columns_list_str} FROM {table_name} WHERE {primary_key} > {max_id}")
-                    rows = pg_cursor.fetchall()
-
-                    with SnowflakeHook(snowflake_conn_id='snowflake').get_conn() as sf_conn:
-                        with sf_conn.cursor() as sf_cursor:
-                            inset_query = f"INSERT INTO {table_name} ({columns_list_str}) VALUES ({placeholders})"
-
-                            for row in rows:
-                                sf_cursor.execute(inset_query, row)
-
-        max_id = get_max_primary_key(table_name)
-        load_incremental_data(table_name, max_id)
+        max_id = get_max_primary_key.override(task_id=f'get_max_id_{table_name}')(table_name)
+        load_incremental_data.override(task_id=f'load_data_{table_name}')(table_name, max_id)
     
 
 postgres_to_snowflake_etl_dag = postgres_to_snowflake_etl()
